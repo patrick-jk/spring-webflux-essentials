be0ffbdbbf8e61c33e9782a681cafce17d1f8606
patrick-jk <patrick.pviolin@senacsp.edu.br>
2024-07-12 11:12:52 -0300

    added service layer and findById

--- src/main/java/academy/devdojo/webflux/controller/AnimeController.java
+++ src/main/java/academy/devdojo/webflux/controller/AnimeController.java
@@ -1,23 +1,30 @@
 package academy.devdojo.webflux.controller;
 
 import academy.devdojo.webflux.domain.Anime;
-import academy.devdojo.webflux.repository.AnimeRepository;
+import academy.devdojo.webflux.service.AnimeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("animes")
 @Slf4j
 public class AnimeController {
-    private final AnimeRepository animeRepository;
+    private final AnimeService animeService;
 
     @GetMapping
     public Flux<Anime> listAll() {
-        return animeRepository.findAll();
+        return animeService.findAll();
+    }
+
+    @GetMapping(path = "{id}")
+    public Mono<Anime> findById(@PathVariable int id) {
+        return animeService.findById(id);
     }
 }
---
+++ src/main/java/academy/devdojo/webflux/service/AnimeService.java
@@ -0,0 +1,30 @@
+package academy.devdojo.webflux.service;
+
+import academy.devdojo.webflux.domain.Anime;
+import academy.devdojo.webflux.repository.AnimeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class AnimeService {
+    private final AnimeRepository animeRepository;
+
+    public Flux<Anime> findAll() {
+        return animeRepository.findAll();
+    }
+
+    public Mono<Anime> findById(int id) {
+        return animeRepository.findById(id)
+                .switchIfEmpty(monoResponseStatusNotFoundException())
+                .log();
+    }
+
+    public <T> Mono<T> monoResponseStatusNotFoundException() {
+        return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime not found"));
+    }
+}
